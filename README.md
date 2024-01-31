cca7aefccbe9b35eed7b9349ab51acd10254b1e0
morris <petri23_@hotmail.com>
2024-01-31 03:28:32 +0200

    cleanup

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-"tesm" 
+## Overview
+todo...
+
+
+## Development
+1.) Clone the repository
+```console
+git clone --recurse-submodules https://github.com/morriskrr/IoT-tesm.git
+```
+
+2.) If not fetching submodules or using older git, do this.
+```console
+git clone https://github.com/morriskrr/IoT-tesm.git
+git submodule update --init --recursive
+```
---
+++ src/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+#define BUFFSIZE 128
+
+
+typedef struct StateMachine
+{
+    int m;
+} StateMachine_t;
+
+
+
+
+// look at the example and read docs
+int ping_COAP_client(char *addr, char *msg)
+{
+    char buffer[BUFFSIZE];
+
+    // 
+
+
+    return 0; // if succ
+}
+
+
+
+
+
+
+int main( void )
+{
+    printf("[*] Sanity check\n");
+
+
+
+
+    return 0;
+}
